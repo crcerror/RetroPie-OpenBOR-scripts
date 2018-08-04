104b06d984fe648800d20ef40d0425553e834d3e
cyperghost <crcerror@users.noreply.github.com>
2018-08-04 02:53:21 +0200

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# PAK EXTRACT v0.55
+# PAK EXTRACT v0.65
 # by cyperghost for retropie.org.uk
 # 1. PLACE  BARE PAK FILES to /home/pi/RetroPie/roms/ports/openbor/pak
 # 2. RUN THE SCRIPT (with user pi!)
@@ -10,30 +10,34 @@ BORROM_DIR="/home/pi/RetroPie/roms/ports/openbor"
 BORPAK_DIR="$BORROM_DIR/pak"
 
 if [[ -f $EXTRACT_BOREXE ]]; then
+
     mkdir -p "$BORPAK_DIR"
     cd "$BORPAK_DIR"
-    FILES="$(ls *.[pP][aA][kK] 2> /dev/null)"
+
+    for i in *.[Pp][Aa][Kk]; do
+
+        FILE="${i%%.*}"
+        if [[ $FILE == '*' ]]; then
+            echo "Aborting... No files to extract in $BORPAK_DIR!"
+            exit
+        fi
+
+        mkdir -p "$BORROM_DIR/$FILE.bor"
+        echo "Extracting file: $i"
+        echo "to dir: $BORROM_DIR/$FILE.bor"
+        sleep 5
+        "$EXTRACT_BOREXE" -d "$BORROM_DIR/$FILE.bor" "$i"
+        echo "-------- Done Extracting: $i ---------"
+        echo "-- Backup $i >> $i.original --"
+        mv "$i" "$i.original"
+        sleep 5
+        
+    done
+
 else
+
     echo "borpak executive file not found in $EXTRACT_BOREXE"
     echo "Exit now...."
     exit
+
 fi
-
-if [[ -z $FILES ]]; then
-    echo "Aborting... No files to extract in $BORPAK_DIR!"
-    exit
-fi
-
-for i in "${FILES[@]}"; do
-    FILE="${i%%.*}"
-
-    mkdir -p "$BORROM_DIR/$FILE.bor"
-    echo "Extracting file: $i"
-    echo "to dir: $BORROM_DIR/$FILE.bor"
-    sleep 3
-    "$EXTRACT_BOREXE" -d "$BORROM_DIR/$FILE.bor" "$i"
-    echo "-------- Done Extracting: $i ---------"
-    echo "-- Backup $i >> $i.original --"
-    mv "$i" "$i.original"
-    sleep 5
-done
